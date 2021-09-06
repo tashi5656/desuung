b862f31e1de320f9e1f5c6ddfbadca10a7ca1c7b
tashi dorji <tashipem5656@gmail.com>
2021-09-06 09:45:41 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const ip = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('content-type', 'text/plain');
+res.end('Hello from server!!');
+});
+
+server.listen(port, ip, () => console.log('server running...'));
+
